e876b1be8aecd8d9f7c77fb54ae1eb6da5c137a7
brunorgdsantos <brunorgdsantos@gmail.com>
2025-08-06 13:35:11 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Heading } from "./components/Heading";
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  return <h1>Hello, World (DO APP!)</h1>; //Return é o que vai ser exibido na página
+  return (
+    <>
+      <Heading />
+      <h1>Hello, World (DO APP!)</h1>
+    </>
+  );
 }
 
 //export default App;
