0edc6845d556a5eb6b43292012520496e5860c21
brunorgdsantos <brunorgdsantos@gmail.com>
2025-08-07 14:32:14 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Heading } from "./components/Heading";
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   return (
     <>
-      <Heading>Ola Mundo de TESTES</Heading>
+      <Heading>
+        Ola Mundo de testes
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <Heading>Ola Mundo de TESTES 2</Heading>
       <Heading>Ola Mundo de TESTES 3</Heading>
       <h1>Hello, World (DO APP!)</h1>
